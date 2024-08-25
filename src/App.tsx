33f8e9c658c6d7661c62fa8954d02ce4018c8e98
Bruno Campos <brunosopmac@gmail.com>
2024-08-25 18:01:11 -0300

    RouterProvider + Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { Provider } from "react-redux"
 import { Container, GlobalStyle } from "./styles"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Home from './pages/Home'
+import Register from "./pages/Register"
+
 import store from "./store"
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+])
 
 function App() {
   return (
     <Provider store={store}>
       <Container>
         <GlobalStyle />
-        
+        <RouterProvider router={routes} />
       </Container>
     </Provider>
   )
